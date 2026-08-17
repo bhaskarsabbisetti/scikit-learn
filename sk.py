5c6be41b7091455f9595ffe7d0308fa8dc7be1ae
bhaskarsabbisetti <bhaskarsabbisetti@gmail.com>
2026-08-17 18:42:14 +0530

    pove

--- sk.py
+++ sk.py
@@ -4,6 +4,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
+from sklearn.metrics import accuracy_score
 model=LinearRegression()
 model2=LogisticRegression()
 X = [
